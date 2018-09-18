030b656f61416de0d2cf7f6e50c530386742716e
Lemail <lemailscrusade@gmail.com>
2018-09-18 16:16:28 +0700

    Initial commit from IDEA

---
+++ src/main/java/DevelopersJdbcDemo.java
@@ -0,0 +1,56 @@
+import java.sql.*;
+
+public class DevelopersJdbcDemo {
+    /**
+     * JDBC Driver and database url
+     */
+    private static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    private static final String DATABASE_URL = "jdbc:mysql://localhost/test?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+
+    /**
+     * User and Password
+     */
+    private static final String USER = "root";
+    private static final String PASSWORD = "кщще";
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Connection connection = null;
+        Statement statement = null;
+
+        System.out.println("Registering JDBC driver...");
+
+        Class.forName("com.mysql.jdbc.Driver");
+
+        System.out.println("Creating database connection...");
+        connection = DriverManager.getConnection(DATABASE_URL, USER, PASSWORD);
+
+        System.out.println("Executing statement...");
+        statement = connection.createStatement();
+
+        String sql;
+        sql = "SELECT * FROM developers";
+
+        ResultSet resultSet = statement.executeQuery(sql);
+
+        System.out.println("Retrieving data from database...");
+        System.out.println("\nDevelopers:");
+        while (resultSet.next()) {
+            int id = resultSet.getInt("id");
+            String name = resultSet.getString("name");
+            String specialty = resultSet.getString("specialty");
+            int salary = resultSet.getInt("salary");
+
+            System.out.println("\n================\n");
+            System.out.println("id: " + id);
+            System.out.println("Name: " + name);
+            System.out.println("Specialty: " + specialty);
+            System.out.println("Salary: $" + salary);
+        }
+
+        System.out.println("Closing connection and releasing resources...");
+        resultSet.close();
+        statement.close();
+        connection.close();
+    }
+}
+
