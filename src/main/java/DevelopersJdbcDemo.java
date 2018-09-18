4ee1e74079a5f96395e33eed74a6df37b3e3dcec
Lemail <lemailscrusade@gmail.com>
2018-09-18 17:58:11 +0700

    Commit from workspace IDEA

--- src/main/java/DevelopersJdbcDemo.java
+++ src/main/java/DevelopersJdbcDemo.java
@@ -1,4 +1,5 @@
 import java.sql.*;
+import java.util.Scanner;
 
 public class DevelopersJdbcDemo {
     /**
@@ -24,13 +25,14 @@ public class DevelopersJdbcDemo {
         System.out.println("Creating database connection...");
         connection = DriverManager.getConnection(DATABASE_URL, USER, PASSWORD);
 
-        System.out.println("Executing statement...");
+        connection.setAutoCommit(false);
+
         statement = connection.createStatement();
 
-        String sql;
-        sql = "SELECT * FROM developers";
+        String SQL;
+        SQL = "SELECT * FROM developers";
 
-        ResultSet resultSet = statement.executeQuery(sql);
+        ResultSet resultSet = statement.executeQuery(SQL);
 
         System.out.println("Retrieving data from database...");
         System.out.println("\nDevelopers:");
@@ -46,8 +48,66 @@ public class DevelopersJdbcDemo {
             System.out.println("Specialty: " + specialty);
             System.out.println("Salary: $" + salary);
         }
+        System.out.println("Creating savepoint...");
+        Savepoint savepointOne = connection.setSavepoint("SavepointOne");
+
+        String batchStatus;
+        Scanner reader = new Scanner(System.in);  // Reading from System.in
+        System.out.println("Do you wish to add new developer into database?(y/n):");
+        batchStatus = reader.nextLine();
+
+
+        while (batchStatus.equals("y")){
+            try {
+                System.out.println("Input id:");
+                int id = reader.nextInt();
+                reader.nextLine();
+                System.out.println("Input name:");
+                String name = reader.nextLine();
+                System.out.println("Input salary:");
+                int salary = reader.nextInt();
+                reader.nextLine();
+                System.out.println("Input speciality:");
+                String specialty = reader.nextLine();
+
+                SQL = "INSERT INTO developers VALUES ("+id+", '"+name+"', '"+specialty+"', "+salary+")";
+                statement.addBatch(SQL);
+
+                System.out.println("Do you wish to add another developer into database?(y/n):");
+                batchStatus = reader.nextLine();
+            } catch (SQLException e) {
+                System.out.println("SQLException. Executing rollback to savepoint...");
+                connection.rollback(savepointOne);
+            }
+        }
+        try{
+            statement.executeBatch();
+            connection.commit();
+        }
+        catch (SQLException e){
+            System.out.println("SQLException. Executing rollback to savepoint...");
+            connection.rollback(savepointOne);
+        }
+
+        SQL = "SELECT * FROM developers";
+        resultSet = statement.executeQuery(SQL);
+        System.out.println("Retrieving data from database...");
+        System.out.println("\nDevelopers:");
+        while (resultSet.next()) {
+            int id = resultSet.getInt("id");
+            String name = resultSet.getString("name");
+            String specialty = resultSet.getString("specialty");
+            int salary = resultSet.getInt("salary");
+
+            System.out.println("id: " + id);
+            System.out.println("Name: " + name);
+            System.out.println("Specialty: " + specialty);
+            System.out.println("Salary: $" + salary);
+            System.out.println("\n================\n");
+        }
 
         System.out.println("Closing connection and releasing resources...");
+        reader.close();
         resultSet.close();
         statement.close();
         connection.close();
